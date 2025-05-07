4339243169b436efe7f23cc09039fa469a943e0d
Szymon <szychoruzy@gmail.com>
2025-05-07 09:57:27 +0200

    polaczenie z db

---
+++ public/db.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private $servername = "mysql";
+    private $username = "v.je";
+    private $password = "v.je";
+    private $dbname = "komunikator";
+    private $conn;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->servername};dbname={$this->dbname};charset=utf8mb4",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo json_encode(['error' => 'Błąd połączenia: ' . $e->getMessage()]);
+            exit;
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+?>
